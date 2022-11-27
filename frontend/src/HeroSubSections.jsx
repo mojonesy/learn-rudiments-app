f353e74864642a67804d006e333b9123cdcea438
Molly <mojonesycreative@gmail.com>
2022-11-27 13:26:54 -0500

    added history section text

--- frontend/src/HeroSubSections.jsx
+++ frontend/src/HeroSubSections.jsx
@@ -7,6 +7,25 @@ export default function HeroSubSections() {
                 <h2 className="article__title article__title__history">
                     The History of Snare Drum Rudiments
                 </h2>
+                <p className="article__body article__body__history">
+                    The 40 snare drum rudiments fall under five categories consisting
+                    of traditional, drum corps, orchestral, European, and contemporary rudiments.
+                    The list was compiled by the Percussive Arts Society (PAS) International Drum
+                    Rudiment Committee in 1984.
+                </p>
+                <p className="article__body article__body__history">
+                    Initially, the National Association of Rudimental Drummers (NARD), established by 
+                    a group that included George Lawrence Stone and William F. Ludwig, Sr., organized
+                    a list of "13 Essential" rudiments plus an additional "13 Rudiments to Complete" in 1933.
+                    These formed the Standard "NARD 26." The "Scottish Drumming Rudiments" sheet includes
+                    46 rudiments, while the "French/Swiss Rudimental Codex" sheet includes 42. Overall, 
+                    there are over 850 rudiments worldwide, but the 40 chosen by PAS are the current standards.
+                    These include 13 "essential," a second set of 13, and a last set 14 fourteen rudiments.
+                </p>
+                <p className="article__body article__body__history">
+                    According to PAS, "all rudiments should be practiced: open (slow) to close (fast)
+                    to open (slow) and/or at an even moderate march tempo."
+                </p>
             </article>
             
             <article className="article article__list">
